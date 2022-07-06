c8095e8a5e7a83acea3998a4889648aa84bed106
Riuzzo01 <davide1.mariuzzo@gmail.com>
2022-07-06 12:04:11 +0200

    Refactoring rock_paper_scissors.js: now the player can choose which move he wants to play

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -2,51 +2,53 @@ function computerPlay() {
     let rand;
     rand = Math.floor(Math.random() * 3);
     if(rand === 0) {
-        return "Rock";
+        return "rock";
     }
     if(rand === 1) {
-        return "Paper";
+        return "paper";
     }
     if(rand === 2) {
-        return "Scissors";
+        return "scissors";
     }
 }
 
 function playRound(playerSelection, computerSelection) {
-    if(playerSelection.localeCompare("Rock")) {
-        if(computerSelection === "Rock") {
-            return "Tie";
+    if(playerSelection.toLowerCase() === "rock") {
+        if(computerSelection === "rock") {
+            return "It's a tie! Either Player and Computer played Rock!";
         }
-        else if(computerSelection === "Paper") {
-            return "Lost";
+        else if(computerSelection === "paper") {
+            return "Player losts! Paper beats Rock!";
         }
         else {
-            return "Win";
+            return "Player wins! Rock beats Scissors!";
         }
     }
-    else if(playerSelection.localeCompare("Paper")) {
-        if(computerSelection === "Paper") {
-            return "Tie";
+    else if(playerSelection.toLowerCase() === "paper") {
+        if(computerSelection === "paper") {
+            return "It's a tie! Either Player and Computer played Paper!";
         }
-        else if(computerSelection === "Rock") {
-            return "Win";
+        else if(computerSelection === "rock") {
+            return "Player wins! Paper beats Rock!";
         }
         else {
-            return "Lost";
+            return "Player losts! Scissors beats Paper!";
         }
     }
-    else {
-        if(computerSelection === "Paper") {
-            return "Win";
+    else  {
+        if(computerSelection === "paper") {
+            return "Player wins! Scissors beats Paper!";
         }
-        else if(computerSelection === "Rock") {
-            return "Lost";
+        else if(computerSelection === "rock") {
+            return "Player losts! Rock beats Scissors!";
         }
         else {
-            return "Tie";
+            return "It's a tie! Either Player and Computer played Scissors!";
         }
     }
 }
 
-const playerSelection = "rock";
-console.log(playRound(playerSelection, computerPlay()));
+let playerSelection = prompt("Choose your move: ");
+let computerSelection = computerPlay();
+console.log("Computer choosed ", computerSelection);
+console.log(playRound(playerSelection, computerSelection));
