5e0cbb1053dbc7e2f89799b5f844c0791081d2ac
Riuzzo01 <davide1.mariuzzo@gmail.com>
2022-07-06 11:42:28 +0200

    Create a function to play a round in 'Rock, Paper, Scissors' in rock_paper_scissors.js

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -11,3 +11,42 @@ function computerPlay() {
         return "Scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection.localeCompare("Rock")) {
+        if(computerSelection === "Rock") {
+            return "Tie";
+        }
+        else if(computerSelection === "Paper") {
+            return "Lost";
+        }
+        else {
+            return "Win";
+        }
+    }
+    else if(playerSelection.localeCompare("Paper")) {
+        if(computerSelection === "Paper") {
+            return "Tie";
+        }
+        else if(computerSelection === "Rock") {
+            return "Win";
+        }
+        else {
+            return "Lost";
+        }
+    }
+    else {
+        if(computerSelection === "Paper") {
+            return "Win";
+        }
+        else if(computerSelection === "Rock") {
+            return "Lost";
+        }
+        else {
+            return "Tie";
+        }
+    }
+}
+
+const playerSelection = "rock";
+console.log(playRound(playerSelection, computerPlay()));
